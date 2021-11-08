6129cb1e8f8cebfa752135bb38e0c96f8a3af019
ostov-larion <39136591+ostov-larion@users.noreply.github.com>
2021-11-09 01:43:29 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,18 +14,28 @@ let md = markdownIt({
     linkify:  true,
     })
     .use(markdownItSpoiler)
-    .use(markdownItHtml5Media.html5Media)
+    .use(markdownItHtml5Media.html5Media, {
+        videoAttrs: "preload=none",
+        audioAttrs: "preload=none"
+    })
 
 const date = () => new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
 
-let post = (msg, num, time) =>
+let post = (msg, files, num, time) =>
     `<div class="post">
         <b>Anonymous</b> <a name="${num}" onclick="document.querySelector('textarea').value += '\\n[#${num}](#${num})'">#${num}</a> <span style="color: gray">${time}</span> <a onclick="mute(${num})">Mute!</a>
+        ${files ? "<br>" + files.map(file => {
+            if(file[1].match(/audio.*/))
+                return `<audio src="${file[0]}" preload="none"></audio>`
+            if(file[1].match(/video.*/))
+                return `<video src="${file[0]}" preload="none"></video>`
+            else return `<img src="${file[0]}"></img>`
+        }).join('') : ""}
         ${md.render(msg)}
     </div>`
 
 let renderPosts = posts => document.querySelector("main").innerHTML =
-    posts.map(({msg, time, poster}, num) => JSON.stringify(localStorage.bl).includes(poster) ? "<div class='post'>(Скрыт)</div>" : post(msg, num, time)).join("")
+    posts.map(({msg, time, poster, files}, num) => JSON.stringify(localStorage.bl).includes(poster) ? "<div class='post'>(Скрыт)</div>" : post(msg, files, num, time)).join("")
 
 update = async() => {
     db = await (await fetch("https://api.jsonstorage.net/v1/json/c6ad7afd-b319-4909-8b41-bc5c6491bd1e", {method: "GET"})).json()
@@ -33,14 +43,41 @@ update = async() => {
     document.querySelectorAll("img").forEach(img => img.onclick = () => window.open(img.src,img.alt,'width=600,height=400'))
 }
 
+uploadFiles = async () => {
+    let cids = []
+    let prgs = 0
+    let files = document.getElementById("files").files
+    let status = document.getElementById("status")
+    let updateStatus = () => status.innerText = files.length != 0 ? `${prgs}/${files.length} uploaded` : ''
+    updateStatus()
+    for await (let file of files) {
+        let cid = await (await fetch("https://api.web3.storage/upload", {
+            method: "POST",
+            headers: {
+                'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGYyMzc3ZDI2YUFFNDdFNDY0NmZFNTVmMDgzMzdlOWQ4Qzk1OUY5YzkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MzYzOTYxNTE0MDIsIm5hbWUiOiJoaXZlY2hhbiJ9.r4Ylo3MGJ9P5ickaWwKWNv8waCTrxgGqxVojBb0yWvA",
+                'Content-Type': file.type,
+            },
+            body: file
+        })).json()
+        console.log(cid)
+        cids.push([cid.cid, file.type])
+        prgs++
+        updateStatus()
+    }
+    document.getElementById("files").value = ''
+    status.innerText = ""
+    return cids.map(([cid, type]) => [`https://ipfs.io/ipfs/${cid}`, type])
+}
+
 uploadPost = async msg => {
+    let files = await uploadFiles()
     await update()
     fetch("https://api.jsonstorage.net/v1/json/c6ad7afd-b319-4909-8b41-bc5c6491bd1e?apiKey=8fb8b49c-e22c-4c05-84a5-caee09624a9d", {
             method: "PUT",
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify([...db, {msg, time: date(), poster: localStorage.poster}])
+            body: JSON.stringify([...db, {msg, files, time: date(), poster: localStorage.poster}])
     }).then(() => update())
 }
 
