eab4b913042a17bf725b1b78551820c53d6e47f4
ostov-larion <39136591+ostov-larion@users.noreply.github.com>
2021-11-08 20:14:37 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,29 +17,31 @@ let md = markdownIt({
     .use(markdownItHtml5Media.html5Media)
 
 
-let post = (msg, num, time) => 
+let post = (msg, num, time) =>
     `<div class="post">
         <b>Anonymous</b> <a name="${num}" onclick="document.querySelector('textarea').value += '\\n[#${num}](#${num})'">#${num}</a> <span style="color: gray">${time}</span> <a onclick="mute(${num})">Mute!</a>
         ${md.render(msg)}
     </div>`
 
-let renderPosts = posts => document.querySelector("main").innerHTML = 
+let renderPosts = posts => document.querySelector("main").innerHTML =
     posts.map(({msg, time, poster}, num) => JSON.stringify(localStorage.bl).includes(poster) ? "<div class='post'>(Скрыт)</div>" : post(msg, num, time)).join("")
 
 update = async() => {
-    db = await (await fetch("/db", {method: "GET"})).json()
+    db = await (await fetch("https://api.jsonstorage.net/v1/json/c6ad7afd-b319-4909-8b41-bc5c6491bd1e", {method: "GET"})).json()
     renderPosts(db.posts)
     document.querySelectorAll("img").forEach(img => img.onclick = () => window.open(img.src,img.alt,'width=600,height=400'))
 }
 
-uploadPost = msg => 
-    fetch("/post", {
-            method: "POST",
+uploadPost = async msg => {
+    await update()
+    fetch("https://api.jsonstorage.net/v1/json/c6ad7afd-b319-4909-8b41-bc5c6491bd1e?apiKey=8fb8b49c-e22c-4c05-84a5-caee09624a9d", {
+            method: "UPDATE",
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({msg, poster: localStorage.poster})
+            body: JSON.stringify([...db, {msg, poster: localStorage.poster}])
     }).then(() => update())
+}
 
 mute = n => {
     localStorage.bl = JSON.stringify(JSON.parse(localStorage.bl).concat([db.posts[n].poster]))
@@ -58,9 +60,5 @@ openStorageWindow = () => {
 }
 
 await update()
-
-document.querySelector("header").innerHTML = `<h1>${db.title}</h1>`
-document.querySelector("title").innerText = db.title + " | Hivechan"
-document.querySelector("dl").innerHTML = `<i>${db.desc}</i>` ?? ""
 
 })()
